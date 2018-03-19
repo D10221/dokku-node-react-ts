3f52eacfe6274a75148fe5b4ac4d98a1f59f8740
Mr D <danieliozzi@gmail.com>
2018-03-19 06:14:48 +0200

    packages: remove jade add explicit dependency on jest/ts-jest test: pass

---
+++ packages/ui/src/app.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+/** */
+const App = () => {
+  return <div className="App">TODO:</div>;
+};
+export default App;
--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,13 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-/** */
-export const App = () => {
-  return (
-    <div className="App">
-      TODO:
-    </div>
-  )
-};
+import App from './app';
+
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
