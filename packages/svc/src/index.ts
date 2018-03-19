a44bd6735767ede5d5ef2110d134c66e4acb6bca
Mr D <danieliozzi@gmail.com>
2018-03-19 06:32:20 +0200

    svc: remove svc favicon dead code

--- packages/svc/src/index.ts
+++ packages/svc/src/index.ts
@@ -12,9 +12,7 @@ import errorHandler from "./error-handler";
 /** */
 const app = express();
 /** Middleware */
-// uncomment after placing your favicon in /public
 app.use(helmet());
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,7 +29,7 @@ const port = Number(process.env.PORT || "5000");
 app.listen(
   port, (error: any) => {
     if (error) {
-      console.error("error: \n", error && error.message ? error.message : JSON.stringify(error));
+      console.error(error && error.message ? error.message : JSON.stringify(error));
       process.exit(-1);
     }
     console.log("public: ", publicDir);
